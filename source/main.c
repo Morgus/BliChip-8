93391a20464ab2fb2ff2715d24360bfd85024c7b
Aleksi Blinnikka <allu2525@gmail.com>
2012-10-08 19:47:31 +0300

    Added a debug function
    Prints out the contents of the memory, CPU registers and stack.
    Memory initialization has now been tested to work properly!

--- source/main.c
+++ source/main.c
@@ -7,19 +7,41 @@
 #include "memory.h"
 #include "keyboard.h"
 
+void PrintDebug(CPU* cpu, Memory* mem)
+{
+	int i;
+	printf("MEMORY:\n\n");
+	for (i = 0; i <= 0xFFF; ++i)
+		printf("%X: %X\n", i, mem->data[i]);
+	printf("CPU REGISTERS:\n\n");
+	for (i = 0; i <= 15; ++i)
+		printf("V%X: %X\n", i, cpu->reg.V[i]);
+	printf("I: %X\n", cpu->reg.I);
+	printf("DT: %X\n", cpu->reg.DT);
+	printf("ST: %X\n", cpu->reg.ST);
+	printf("PC: %X\n", cpu->reg.PC);
+	printf("SP: %X\n", cpu->reg.SP);
+	printf("STACK:\n\n");
+	for (i = 0; i <= 15; ++i)
+		printf("%X: %X\n", i, cpu->stack[i]);
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc != 2) {
 		printf("Wrong arguments.");
 		return 1;
 	}
+	int running = 1;
 	CPU* cpu;
 	Memory* mem;
 	Keyboard* keyb;
 	cpu = CreateCPU();
 	mem = CreateMemory(argv[1]);
 	keyb = CreateKeyboard();
-	
+	// Test, prints the contents of the memory and registers
+	PrintDebug(cpu, mem);
+	/////////////
 	DoCPUCycle(cpu, mem, keyb);
 	
 	DestroyKeyboard(keyb);
