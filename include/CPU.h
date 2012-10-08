7fd474098cfd687fced794ad9c8212abffd6f41e
Aleksi Blinnikka <allu2525@gmail.com>
2012-10-08 22:58:37 +0300

    Created opcode definitions

--- include/CPU.h
+++ include/CPU.h
@@ -10,6 +10,7 @@
 #include <stdint.h>
 #include "memory.h"
 #include "keyboard.h"
+#include "display.h"
 
 typedef struct _Registers
 {
@@ -38,6 +39,42 @@ typedef struct _CPU
 	Registers reg;
 	uint16_t stack[16];
 } CPU;
+
+void Op_0NNN(uint16_t opcode, CPU* cpu);
+void Op_00E0(uint16_t opcode, CPU* cpu, Display* disp);
+void Op_00EE(uint16_t opcode, CPU* cpu);
+void Op_1NNN(uint16_t opcode, CPU* cpu);
+void Op_2NNN(uint16_t opcode, CPU* cpu);
+void Op_3XNN(uint16_t opcode, CPU* cpu);
+void Op_4XNN(uint16_t opcode, CPU* cpu);
+void Op_5XY0(uint16_t opcode, CPU* cpu);
+void Op_6XNN(uint16_t opcode, CPU* cpu);
+void Op_7XNN(uint16_t opcode, CPU* cpu);
+void Op_8XY0(uint16_t opcode, CPU* cpu);
+void Op_8XY1(uint16_t opcode, CPU* cpu);
+void Op_8XY2(uint16_t opcode, CPU* cpu);
+void Op_8XY3(uint16_t opcode, CPU* cpu);
+void Op_8XY4(uint16_t opcode, CPU* cpu);
+void Op_8XY5(uint16_t opcode, CPU* cpu);
+void Op_8XY6(uint16_t opcode, CPU* cpu);
+void Op_8XY7(uint16_t opcode, CPU* cpu);
+void Op_8XYE(uint16_t opcode, CPU* cpu);
+void Op_9XY0(uint16_t opcode, CPU* cpu);
+void Op_ANNN(uint16_t opcode, CPU* cpu);
+void Op_BNNN(uint16_t opcode, CPU* cpu);
+void Op_CXNN(uint16_t opcode, CPU* cpu);
+void Op_DXYN(uint16_t opcode, CPU* cpu, Memory* mem, , Display* disp);
+void Op_EX9E(uint16_t opcode, CPU* cpu, Keyboard* keyb);
+void Op_EXA1(uint16_t opcode, CPU* cpu, Keyboard* keyb);
+void Op_FX07(uint16_t opcode, CPU* cpu);
+void Op_FX0A(uint16_t opcode, CPU* cpu, Keyboard* keyb);
+void Op_FX15(uint16_t opcode, CPU* cpu);
+void Op_FX18(uint16_t opcode, CPU* cpu);
+void Op_FX1E(uint16_t opcode, CPU* cpu);
+void Op_FX29(uint16_t opcode, CPU* cpu, Memory* mem);
+void Op_FX33(uint16_t opcode, CPU* cpu, Memory* mem);
+void Op_FX55(uint16_t opcode, CPU* cpu, Memory* mem);
+void Op_FX65(uint16_t opcode, CPU* cpu, Memory* mem);
 
 void DoCPUCycle(CPU* cpu, Memory* mem, Keyboard* keyb);
 void UpdateCPUTimers(CPU* cpu);
