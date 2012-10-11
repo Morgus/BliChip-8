667425d2fbc7fb9283275ecbded86e3303d1ed59
Aleksi Blinnikka <allu2525@gmail.com>
2012-10-11 20:28:54 +0300

    Creaeted the main loop

--- source/main.c
+++ source/main.c
@@ -9,13 +9,10 @@
 #include "keyboard.h"
 #include "display.h"
 
-void PrintDebug(CPU* cpu, Memory* mem, Display* disp)
+void PrintDebug(CPU* cpu, Display* disp, Keyboard* keyb)
 {
 	int i, j;
-	printf("MEMORY:\n\n");
-	for (i = 0; i <= 0xFFF; ++i)
-		printf("%X: %X\n", i, mem->data[i]);
-	printf("\nCPU REGISTERS:\n\n");
+	printf("CPU REGISTERS:\n\n");
 	for (i = 0; i <= 15; ++i)
 		printf("V%X: %X\n", i, cpu->reg.V[i]);
 	printf("I: %X\n", cpu->reg.I);
@@ -32,6 +29,9 @@ void PrintDebug(CPU* cpu, Memory* mem, Display* disp)
 			printf("%i", disp->pixel[i][j]);
 		printf("\n");
 	}
+	printf("\nKEYBOARD:\n\n");
+	for (i = 0; i <= 15; ++i)
+		printf("%X: %i\n", i, keyb->key[i]);
 }
 
 int main(int argc, char* argv[])
@@ -44,30 +44,29 @@ int main(int argc, char* argv[])
 	if (SDL_INIT_VIDEO < 0)
 		return 1;
 	
-	int running = 1;
+	SDL_Event ev;
+	
 	CPU* cpu;
 	Memory* mem;
 	Keyboard* keyb;
 	Display* disp;
+	
 	cpu = CreateCPU();
 	mem = CreateMemory(argv[1]);
 	keyb = CreateKeyboard();
 	disp = CreateDisplay();
 	
-	UpdateDisplay(disp);
-	// Test, only 62 cycles
-	for (running = 0; running < 62; ++running) {
+	while (cpu->running) {
+		while (SDL_PollEvent(&ev)) {
+			if (ev.type == SDL_QUIT)
+				cpu->running = 0;
+		}
 		UpdateKeyboard(keyb);
 		DoCPUCycle(cpu, mem, keyb, disp);
 		cpu->reg.PC += 2;
 	}
-	//while (running) {
-	//	DoCPUCycle(cpu, mem, keyb, disp);
-	//	cpu->reg.PC += 2;
-	//	running = 0;
-	//}
 	// Test, prints the contents of the memory and registers
-	PrintDebug(cpu, mem, disp);
+	PrintDebug(cpu, disp, keyb);
 	/////////////
 	
 	DestroyDisplay(disp);
