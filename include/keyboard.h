c106b8c93fe478067e6bf9d101277f2523e4538a
Aleksi Blinnikka <allu2525@gmail.com>
2012-10-11 20:29:11 +0300

    Implemented keyboard with SDL

--- include/keyboard.h
+++ include/keyboard.h
@@ -5,12 +5,22 @@
 #ifndef _KEYBOARD_H_
 #define _KEYBOARD_H_
 
+#include <SDL.h>
 #include <stdlib.h>
 #include <stdint.h>
+
+typedef enum _Keys
+{
+	KEY_1 = SDL_SCANCODE_1, KEY_2 = SDL_SCANCODE_2, KEY_3 = SDL_SCANCODE_3, KEY_C = SDL_SCANCODE_4,
+	KEY_4 = SDL_SCANCODE_Q, KEY_5 = SDL_SCANCODE_W, KEY_6 = SDL_SCANCODE_E, KEY_D = SDL_SCANCODE_R,
+	KEY_7 = SDL_SCANCODE_A, KEY_8 = SDL_SCANCODE_S, KEY_9 = SDL_SCANCODE_D, KEY_E = SDL_SCANCODE_F,
+	KEY_A = SDL_SCANCODE_Z, KEY_0 = SDL_SCANCODE_X, KEY_B = SDL_SCANCODE_C, KEY_F = SDL_SCANCODE_V,
+} Keys;
 
 typedef struct _Keypad
 {
 	uint8_t key[16];
+	Uint8* state;
 } Keyboard;
 
 void UpdateKeyboard(Keyboard* keyb);
--- source/keyboard.c
+++ source/keyboard.c
@@ -6,6 +6,87 @@
 
 void UpdateKeyboard(Keyboard* keyb)
 {
+	keyb->state = SDL_GetKeyboardState(NULL);
+	
+	if (keyb->state[KEY_0])
+		keyb->key[0x0] = 1;
+	else
+		keyb->key[0x0] = 0;
+	
+	if (keyb->state[KEY_1])
+		keyb->key[0x1] = 1;
+	else
+		keyb->key[0x1] = 0;
+	
+	if (keyb->state[KEY_2])
+		keyb->key[0x2] = 1;
+	else
+		keyb->key[0x2] = 0;
+	
+	if (keyb->state[KEY_3])
+		keyb->key[0x3] = 1;
+	else
+		keyb->key[0x3] = 0;
+	
+	if (keyb->state[KEY_4])
+		keyb->key[0x4] = 1;
+	else
+		keyb->key[0x4] = 0;
+	
+	if (keyb->state[KEY_5])
+		keyb->key[0x5] = 1;
+	else
+		keyb->key[0x5] = 0;
+	
+	if (keyb->state[KEY_6])
+		keyb->key[0x6] = 1;
+	else
+		keyb->key[0x6] = 0;
+	
+	if (keyb->state[KEY_7])
+		keyb->key[0x7] = 1;
+	else
+		keyb->key[0x7] = 0;
+	
+	if (keyb->state[KEY_8])
+		keyb->key[0x8] = 1;
+	else
+		keyb->key[0x8] = 0;
+	
+	if (keyb->state[KEY_9])
+		keyb->key[0x9] = 1;
+	else
+		keyb->key[0x9] = 0;
+	
+	if (keyb->state[KEY_A])
+		keyb->key[0xA] = 1;
+	else
+		keyb->key[0xA] = 0;
+	
+	if (keyb->state[KEY_B])
+		keyb->key[0xB] = 1;
+	else
+		keyb->key[0xB] = 0;
+	
+	if (keyb->state[KEY_C])
+		keyb->key[0xC] = 1;
+	else
+		keyb->key[0xC] = 0;
+	
+	if (keyb->state[KEY_D])
+		keyb->key[0xD] = 1;
+	else
+		keyb->key[0xD] = 0;
+	
+	if (keyb->state[KEY_E])
+		keyb->key[0xE] = 1;
+	else
+		keyb->key[0xE] = 0;
+	
+	if (keyb->state[KEY_F])
+		keyb->key[0xF] = 1;
+	else
+		keyb->key[0xF] = 0;
 }
 
 Keyboard* CreateKeyboard()
